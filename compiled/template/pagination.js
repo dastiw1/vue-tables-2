7ca65337f38c740904bad55bfc450411275e85bb
Home/Dastan <seer.dastan@gmail.com>
2017-09-14 14:06:54 +0600

    via vue-table you can change records count to pages count. It compotibility commit

--- compiled/template/pagination.js
+++ compiled/template/pagination.js
@@ -1,24 +1,28 @@
 "use strict";
 
 module.exports = function (h, that) {
-  if (that.opts.pagination && !that.opts.pagination.dropdown) {
+    if (that.opts.pagination && !that.opts.pagination.dropdown) {
 
-    var name = that.vuex ? that.name : that.id;
+        var name = that.vuex ? that.name : that.id;
 
-    return h(
-      "pagination",
-      {
-        ref: "pagination",
-        attrs: { "for": name,
-          vuex: that.vuex,
-          records: that.count,
-          "per-page": parseInt(that.limit),
-          chunk: that.opts.pagination.chunk,
-          "count-text": that.opts.texts.count }
-      },
-      []
-    );
-  }
+        return h(
+            "pagination",
+            {
+                ref: "pagination",
+                attrs: {
+                    "for": name,
+                    vuex: that.vuex,
+                    records: that.count,
+                    "per-page": parseInt(that.limit),
+                    chunk: that.opts.pagination.chunk,
+                    "count-text": that.opts.texts.count,
+                    "pages-count-text": that.opts.texts.pagesCount,
+                    "count-text-type": that.opts.pagination.textType,
+                }
+            },
+            []
+        );
+    }
 
-  return '';
+    return '';
 };
